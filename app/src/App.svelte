440f9795ff67e4a44eecf423ac781ae66ef3e82c
Steve Roe <steve@phramesco.com>
2020-04-21 18:44:37 +0900

    first commit

---
+++ app/src/App.svelte
@@ -0,0 +1,50 @@
+<script>
+	import Navbar from './Navbar.svelte'
+	import Player from './Player.svelte'
+	import AddPlayer from './AddPlayer.svelte'
+
+
+	let players = [
+		{
+			name: "John",
+			points: 53
+		},
+		{
+			name: "Simon",
+			points: 23
+		},
+		{
+			name: "Dan",
+			points: 83
+		},
+	]
+
+	const addPlayer = e => {
+		console.log(e.detail)
+		const newPlayer =  e.detail
+		players = [...players, newPlayer]
+	}
+
+	const removePlayer = e => {
+		players = players.filter(player => player.name !== e.detail)
+	}
+</script>
+
+<main>
+<Navbar />
+	<div class="container">
+	<AddPlayer on:addplayer={addPlayer} />
+		{#if players.length === 0}
+			<p>No Players</p>
+		{:else}
+			{#each players as player}
+				<Player 
+					name={player.name} 
+					points={player.points} 
+					on:removeplayer={removePlayer}
+				/>
+			{/each}
+		{/if}
+	</div>
+</main>
+ 
---
+++ app/src/Player.svelte
@@ -0,0 +1,31 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+    const dispatch = createEventDispatcher()
+	export let name 
+	export let points
+	let showControls = false
+
+	const addPoint = () => (points += 1)
+	const removePoint = () => (points -= 1)
+	const toggleControls = () => (showControls = !showControls)
+    const onDelete = () => dispatch("removeplayer", name)
+</script>
+
+<main>
+
+
+		<div class="card">
+			<h1>{name}</h1>
+			<button class="btn btn-sm" on:click={toggleControls}>
+				{#if showControls}-{:else}+{/if}
+			</button>
+            <button class="btn btn-danger btn-sm" on:click={onDelete}>x</button>
+			<h3>Points: {points}</h3>
+			{#if showControls}
+				<button class="btn" on:click={addPoint}>+1</button>
+				<button class="btn" on:click={removePoint}>-1</button>
+				<input type="number" bind:value={points} />
+			{/if}
+		</div>
+</main>
+ 
